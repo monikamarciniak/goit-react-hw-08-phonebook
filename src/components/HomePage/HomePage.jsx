ef50e3bc2eb306fceadb8ba34683f4359a8beb91
Monika Marciniak <monixxa90@gmail.com>
2022-12-27 17:55:34 +0100

    changes

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -5,7 +5,7 @@ export const HomePage = () => {
   return (
     <Container className={styles.container}>
       <h1 className="text-center">
-        Welcome to Phonebook Application &#128214;
+        Welcome to Phonebook Application
       </h1>
     </Container>
   );
